3c273d64d721de246628a91bc79bd3b99d95f417
tg <T.Gautier@gys.fr>
2024-06-20 15:46:18 +0200

    remove console log

--- common/middleware/authentication.js
+++ common/middleware/authentication.js
@@ -8,7 +8,6 @@ const authentication = (req, res, next) => {
         res.status(401).end();
     } else {
         const credentials = new Buffer.from(authheader.split(' ')[1], 'base64').toString();
-        console.log(credentials)
         const [user, pwd] = credentials.split(':');
         if (pwd === admin.ADMIN_KEY && user === admin.ADMIN_USER) {
             next();
